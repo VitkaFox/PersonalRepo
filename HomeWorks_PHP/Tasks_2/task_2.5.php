8e7d1cf2650b81e48af19cfa303008a997f2fdb4
VitkaFox <36963396+VitkaFox@users.noreply.github.com>
2018-05-03 12:53:07 +0300

    Add files via upload

--- HomeWorks_PHP/Tasks_2/task_2.5.php
+++ HomeWorks_PHP/Tasks_2/task_2.5.php
@@ -55,6 +55,15 @@ for ($i = 0; $i < $x; $i++) {
 echo "<h2>Массив начальный</h2><pre>" . print_r($fruits, 1) . "</pre>";
 
 $unique_fruits = array_unique($fruits, SORT_REGULAR);
+$y = count($unique_fruits);
+for ($i = 0; $i < $y; $i++) {
+    $unique_fruits[$i]["count"] = 0;
+    for ($j = 0; $j < $y; $j++) {
+        if ($unique_fruits[$i]["name"] == $unique_fruits[$j]["name"]) {
+            $unique_fruits[$i]["count"] += 1;
+        }
+    }
+}
 echo "<h2>Массив конечный</h2><pre>" . print_r($unique_fruits, 1) . "</pre>";
 
 
